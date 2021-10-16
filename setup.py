ff4540629659cc3481740fa7682a7cc50cc8dc3c
Andrew Kane <andrew@ankane.org>
2021-10-15 20:01:52 -0700

    Added long description [skip ci]

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='hightop',
     version='0.1.0',
     description='A nice shortcut for group count queries with Django',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ankane/hightop.py',
     author='Andrew Kane',
     author_email='andrew@ankane.org',
